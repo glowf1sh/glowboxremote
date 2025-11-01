b8f71e08a7ac9c94ca3be550d65a5269d3a2f379
glowf1sh / https://twitch.tv/glowf1sh <noreply@github.com>
2025-11-01 20:53:40 +0100

    Update Python modules and manifest
    - Update module initialization and structure
    - Add manifest.json with file checksums (v2.0.0)
    - Improve module import handling
    - Update .gitignore configuration
    - Remove deprecated scripts

--- belabox-api/_core/pyarmor_runtime_011004/__init__.py
+++ belabox-api/_core/pyarmor_runtime_011004/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.9 (pro), 011004, 2025-11-01T02:18:00.828723
+# Pyarmor 9.1.9 (pro), 011004, 2025-11-01T20:52:43.806956
 from .pyarmor_runtime import __pyarmor__
--- rist/_core/pyarmor_runtime_011004/__init__.py
+++ rist/_core/pyarmor_runtime_011004/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.9 (pro), 011004, 2025-11-01T02:19:23.810047
+# Pyarmor 9.1.9 (pro), 011004, 2025-11-01T20:52:51.675297
 from .pyarmor_runtime import __pyarmor__
