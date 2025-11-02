92ef31c8940a605eeafefbccc42b37ce62fc034a
glowf1sh / https://twitch.tv/glowf1sh <noreply@github.com>
2025-11-02 14:23:57 +0100

    Update installer and Python modules
    - Fix module initialization
    - Improve installer validation
    - Update module structure

--- belabox-api/_core/pyarmor_runtime_011004/__init__.py
+++ belabox-api/_core/pyarmor_runtime_011004/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.9 (pro), 011004, 2025-11-02T13:57:08.786806
+# Pyarmor 9.1.9 (pro), 011004, 2025-11-02T14:22:49.234318
 from .pyarmor_runtime import __pyarmor__
--- rist/_core/pyarmor_runtime_011004/__init__.py
+++ rist/_core/pyarmor_runtime_011004/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.9 (pro), 011004, 2025-11-02T13:57:21.225589
+# Pyarmor 9.1.9 (pro), 011004, 2025-11-02T14:23:19.228183
 from .pyarmor_runtime import __pyarmor__
