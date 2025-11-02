7b8e0ff3c7c872112c720186a6d267906a542864
glowf1sh / https://twitch.tv/glowf1sh <noreply@github.com>
2025-11-02 02:38:17 +0100

    Update system modules and configuration
    - Improve module initialization
    - Update configuration handling
    - Enhance manifest structure (v2.0.1)
    - Refactor import paths

--- belabox-api/_core/pyarmor_runtime_011004/__init__.py
+++ belabox-api/_core/pyarmor_runtime_011004/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.9 (pro), 011004, 2025-11-01T20:52:43.806956
+# Pyarmor 9.1.9 (pro), 011004, 2025-11-02T02:38:02.170402
 from .pyarmor_runtime import __pyarmor__
--- rist/_core/pyarmor_runtime_011004/__init__.py
+++ rist/_core/pyarmor_runtime_011004/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.9 (pro), 011004, 2025-11-01T20:52:51.675297
+# Pyarmor 9.1.9 (pro), 011004, 2025-11-02T02:38:02.900102
 from .pyarmor_runtime import __pyarmor__
