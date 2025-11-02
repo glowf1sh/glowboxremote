efbc65fdaec7246fc9c3d7201b4683c07c0ac1cc
glowf1sh / https://twitch.tv/glowf1sh <noreply@github.com>
2025-11-02 13:21:27 +0100

    Add installation script support to manifest

--- belabox-api/_core/pyarmor_runtime_011004/__init__.py
+++ belabox-api/_core/pyarmor_runtime_011004/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.9 (pro), 011004, 2025-11-02T13:08:56.987954
+# Pyarmor 9.1.9 (pro), 011004, 2025-11-02T13:20:22.308921
 from .pyarmor_runtime import __pyarmor__
--- rist/_core/pyarmor_runtime_011004/__init__.py
+++ rist/_core/pyarmor_runtime_011004/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.9 (pro), 011004, 2025-11-02T13:09:06.878329
+# Pyarmor 9.1.9 (pro), 011004, 2025-11-02T13:20:34.262128
 from .pyarmor_runtime import __pyarmor__
